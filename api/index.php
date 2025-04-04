cada222267a2d6e9d66105299ab6ea65400decd0
Latif Falikal <131035184+Latiffhl@users.noreply.github.com>
2025-04-04 13:34:57 +0700

    v2.3

--- api/index.php
+++ api/index.php
@@ -1,1 +1,23 @@
-<?php __DIR__ . "../public/index.php"; ?>
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+define('BASE_PATH', __DIR__);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lartz</title>
+
+    <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <main>
+      <article>
+        <section id="hero">
+          <?php include BASE_PATH . '/pages/hero.php'; ?>
+        </section>
+      </article>
+    </main>
+  </body>
+</html>
