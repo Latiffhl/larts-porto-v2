07e469392f2c1f53c6655d378779d978525f95eb
Latif Falikal <131035184+Latiffhl@users.noreply.github.com>
2025-04-04 14:33:33 +0700

    v3.1

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
-$indexFilePath = __DIR__ . '/../public/index.html';
+header('Content-Type: text/html; charset=UTF-8');
 
+// Dynamically resolve the path to the public/index.html file
+$indexFilePath = realpath(__DIR__ . '/../public/index.html');
+
+// Check if the file exists
+if ($indexFilePath && file_exists($indexFilePath)) {
+    // Read and output the contents of the file
+    readfile($indexFilePath);
+} else {
+    // Return a 404 error if the file is not found
+    http_response_code(404);
+    echo "<h1>404 Not Found</h1><p>The requested file could not be found.</p>";
+}
+?>
