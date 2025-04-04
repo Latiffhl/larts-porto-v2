3b968ba249683d10c466c680d4dde7fe02a21450
Latif Falikal <131035184+Latiffhl@users.noreply.github.com>
2025-04-04 14:38:20 +0700

    v3.2

--- api/index.php
+++ api/index.php
@@ -1,13 +1,13 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 
-// Dynamically resolve the path to the public/index.html file
-$indexFilePath = realpath(__DIR__ . '/../public/index.html');
+// Dynamically resolve the path to the public/index.php file
+$indexFilePath = realpath(__DIR__ . '/../public/index.php');
 
 // Check if the file exists
 if ($indexFilePath && file_exists($indexFilePath)) {
-    // Read and output the contents of the file
-    readfile($indexFilePath);
+    // Include and execute the file
+    require $indexFilePath;
 } else {
     // Return a 404 error if the file is not found
     http_response_code(404);
