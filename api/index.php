2e9202eb938b032ac8e773ad6ee9a03a561292c2
Latif Falikal <131035184+Latiffhl@users.noreply.github.com>
2025-04-04 12:30:35 +0700

    v3

--- api/index.php
+++ api/index.php
@@ -7,31 +7,30 @@
     <main>
       <article>
         <section id="hero">
-          <?php include __DIR__ . '/pages/hero/hero.php'; ?>
+          <?php include '/var/task/user/api/pages/hero/hero.php'; ?>
         </section>
 
         <section id="about">
-          <?php include __DIR__ . '/pages/about/about.php'; ?>
+          <?php include '/var/task/user/api/pages/about/about.php'; ?>
         </section>
 
         <section id="portfolio">
-          <?php include __DIR__ . '/pages/portofolio/portofolio.php'; ?>
+          <?php include '/var/task/user/api/pages/portofolio/portofolio.php'; ?>
         </section>
 
         <section id="skills">
-          <?php include __DIR__ . '/pages/skills/skills.php'; ?>
+          <?php include '/var/task/user/api/pages/skills/skills.php'; ?>
         </section>
 
         <section id="blog">
-          <?php include __DIR__ . '/pages/blogs/blogs.php'; ?>
+          <?php include '/var/task/user/api/pages/blogs/blogs.php'; ?>
         </section>
 
         <section id="contact">
-          <?php include __DIR__ . '/pages/contact/contact.php'; ?>
+          <?php include '/var/task/user/api/pages/contact/contact.php'; ?>
         </section>
       </article>
     </main>
     <!-- ...existing code... -->
   </body>
 </html>
-
